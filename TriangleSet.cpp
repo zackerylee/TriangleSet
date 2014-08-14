c2bd562885a492787c7c3a418ace0c0bb09ed726
Zack Douglas <zackerylee@gmail.com>
2014-08-13 23:46:15 -0600

    change to the TriplexWwriter

--- TriangleSet.cpp
+++ TriangleSet.cpp
@@ -2,35 +2,42 @@
 
 
 
-TriangleSet::TriangleSet(int rPin, int gPin, int bPin) {
+TriangleSet::TriangleSet(int rPin, int gPin, int bPin, TriplexWriter &triplexWriter) {
 	_rPin = rPin;
 	_gPin = gPin;
 	_bPin = bPin;
+	writer = triplexWriter;
 }
 
 void TriangleSet::goRed() {
-	analogWrite(_rPin, 0);
-	analogWrite(_gPin, 255);
-	analogWrite(_bPin, 255);
+	writer.write(_rPin, 0);
+	writer.write(_gPin, 255);
+	writer.write(_bPin, 255);
 }
 void TriangleSet::goBlue() {
-	analogWrite(_rPin, 255);
-	analogWrite(_gPin, 255);
-	analogWrite(_bPin, 0);
+	writer.write(_rPin, 255);
+	writer.write(_gPin, 255);
+	writer.write(_bPin, 0);
 }
 void TriangleSet::goGreen() {
-	analogWrite(_rPin, 255);
-	analogWrite(_gPin, 0);
-	analogWrite(_bPin, 255);
+	writer.write(_rPin, 255);
+	writer.write(_gPin, 0);
+	writer.write(_bPin, 255);
 }
 void TriangleSet::goFull() {
-	analogWrite(_rPin, 255);
-	analogWrite(_gPin, 255);
-	analogWrite(_bPin, 255);
+	writer.write(_rPin, 255);
+	writer.write(_gPin, 255);
+	writer.write(_bPin, 255);
 }
 void TriangleSet::goZero() {
-	analogWrite(_rPin, 0);
-	analogWrite(_gPin, 0);
-	analogWrite(_bPin, 0);
+	writer.write(_rPin, 0);
+	writer.write(_gPin, 0);
+	writer.write(_bPin, 0);
+}
+
+void TriangleSet::manualWrite(int red, int green, int blue){
+	writer.write(_rPin, red);
+	writer.write(_gPin, green);
+	writer.write(_bPin, blue);
 }
 
--- TriangleSet.h
+++ TriangleSet.h
@@ -1,21 +1,24 @@
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
+#ifndef TriangleSet_H
+#define TriangleSet_H
+
+#include "TriplexWriter.h"
 
 class TriangleSet {
 public:
-	TriangleSet(int rPin, int gPin, int bPin);
+	TriangleSet(int rPin, int gPin, int bPin, TriplexWriter &triplexWriter);
 	void goRed();
 	void goBlue();
 	void goGreen();
 	void goFull();
 	void goZero();
+	void manualWrite(int red, int green, int blue);
 
 private:
 	int _rPin;
 	int _gPin;
 	int _bPin;
+	TriplexWriter writer;
 };
+
+#endif
